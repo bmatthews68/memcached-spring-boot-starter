5422c37c2f2702eb93697cb6db0777841dec199c
Brian Matthews <brian.matthews@synchronoss.com>
2017-08-19 23:35:19 +0100

    Updated copyright headers and set Spring Boot version to 1.5.6

--- src/main/java/com/btmatthews/springboot/memcached/MemcachedAutoConfiguration.java
+++ src/main/java/com/btmatthews/springboot/memcached/MemcachedAutoConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 Brian Matthews
+ * Copyright 2016-2017 Brian Matthews
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
